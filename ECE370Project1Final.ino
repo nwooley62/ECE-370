58632f5f63da86b96ffc0468895a9fcb98a8dcdb
nwooley62 <47373773+nwooley62@users.noreply.github.com>
2019-03-26 18:20:54 -0400

    ECE370Project1Final
    Added in pControl loop and Raspberry pi serial connection

--- ECE370Project1Final.ino
+++ ECE370Project1Final.ino
@@ -1,184 +1,83 @@
-int pinA = 5;
-int pinB = 9;
-int IR = 13;
-float requested;
-int numTicksCounted, numTicksNeeded;
-float currentAngle, desiredAngle, angleChange;
-float timeStart, timeNow, timeDelay = 100;
-boolean slowDown = false;
+#define pinA 5
+#define pinB 9
+#define IRA 13
+#define IRB 12
+
+float requested = 0;
+int numTicksCounted = 0;
+float currentAngle = 0;
+int IRoutA, IRoutB;
+int time1, time2;
+float Kp = 7;
+float error, output, angle, motor;
+float t1, t2;
+int incoming[6];
+int sign, modSum, divSum, checkSum;
+int dir;
 void setup() {
 
   Serial.begin(9600);
+  Serial1.begin(9600);
   pinMode(pinA, OUTPUT);
   pinMode(pinB, OUTPUT);
-  pinMode(IR, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(IR), getTick, CHANGE);
+  pinMode(IRB, INPUT);
+  pinMode(IRA, INPUT);
+  attachInterrupt(digitalPinToInterrupt(IRA), getTick, CHANGE);
   currentAngle = 0;
+  t1 = millis();
+//  for(int i = 0; i < 7; i++){
+//    incoming[i] = 0;
+//  }
 }
 ////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {
-
-  while(1){
-    if(Serial.available()){
-      requested = Serial.parseFloat();
-      Serial.println(requested);
-      numTicksCounted = 0;
-      setServo(requested);
-      slowDown = false;
+while(true){
+  if(Serial1.available() >= 6){
+    for(int i = 0; i < 7; i++){
+      incoming[i] = (Serial1.read());
+      Serial.println(incoming[i]);
     }
   }
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////
-void stopServo(){
-  if(slowDown == true){
-    detachInterrupt(digitalPinToInterrupt(IR));
-    Serial.println("check 1");
-    currentAngle = desiredAngle;                              //save motor position
-
-   /* if(angleChange > 0){                                    //Stop motor momentum
-      setVelocity(25);
+  if(incoming[0] == 255 && incoming[1] == 255){
+    //Serial.println("All clear first two bytes");
+    sign = incoming[2];
+    divSum = incoming[3] * 256;
+    modSum = incoming[4];
+    checkSum = 255 % (sign + divSum + modSum);
+    requested = divSum + modSum;
+    if(checkSum == incoming[5]){
+      if(sign > 0){
+        requested = abs(requested);
+        Serial.println(requested);
+      }else{
+          requested = -1 * abs(requested);
+        Serial.println(requested);
+      }
     }
-    else if(angleChange < 0){                                 //need to figure out how to delay the motors so they can stop properly. All solutions give an error when reading from serial device in PuTTy
-      setVelocity(250);                                       //have tried a delay(100), a loop of 100 delay(1), and a millis() loop. Using delay() works, however PuTTY throws an error every time
-    }
-                                                              
-    delay(100);*/
-    analogWrite(pinA, 0);                                     //Stop motors completely
-    analogWrite(pinB, 0);
-    attachInterrupt(digitalPinToInterrupt(IR), getTick, CHANGE);
   }
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////
-void setServo(float degree){
-
-  if(degree > 720){                                           //check if input degrees is greater than +- 720
-    degree = 720;
-  }else if(degree < -720){
-    degree = -720;
-  }
-  desiredAngle = degree;
-  angleChange = desiredAngle - currentAngle;                  //get the distance that the motor needs to travel
+  if(requested > 720){ requested = 720;}
+  else if(requested < -720) {requested = -720;}
+  currentAngle = getAngle(numTicksCounted);
   
-  numTicksNeeded = abs(angleChange / 360.0) * 75.8 * 4.0;     //set degrees to ticks
-  
-  if(angleChange > 0){                                        //set velocity based on direciton of travel
-    setVelocity(250);
-  }
-  else if(angleChange < 0){
-    setVelocity(5);
-  }
-  return;
+  t1 = millis();
+  //if(t1 - t2 >= 20){
+    motor = pControl(requested, currentAngle) + 127;
+    setVelocity(motor);
+    t2 = t1;
+  //}
 }
-
+}
 ///////////////////////////////////////////////////////////////////////////////////////////
 void getTick(){
 
-  numTicksCounted++;                                          //increment the number of ticks travelled
-  
-  
-  if(angleChange <= -540){                                       //Since the reverse direction timing is not working, trying the solution of stopping short every time, by different amounts
-    if(numTicksCounted > (numTicksNeeded - 260) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    }
-  }else if(angleChange <= -360){
-    if(numTicksCounted > (numTicksNeeded - 200) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    }
-  }else if(angleChange <= -270){
-    if(numTicksCounted > (numTicksNeeded - 150) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    }
-  }else if(angleChange <= -180){
-    if(numTicksCounted > (numTicksNeeded - 110) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    }
-  }else if(angleChange <= -150){
-    if(numTicksCounted > (numTicksNeeded - 100) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    }
-  }else if(angleChange <= -135){
-    if(numTicksCounted > (numTicksNeeded - 90) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    }
-  }else if(angleChange <= -90){
-    if(numTicksCounted > (numTicksNeeded - 60) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    }
-  }else if(angleChange <= -60){
-    if(numTicksCounted > (numTicksNeeded - 20) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    }
-  }else if(angleChange <= -30){
-   if(numTicksCounted > (numTicksNeeded - 0) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    } 
-  }else if(angleChange < -15){
-    if(numTicksCounted > (numTicksNeeded - 0) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    } 
-  }else if(angleChange < 15){                                     
-    if(numTicksCounted > (numTicksNeeded - 0) && slowDown == false){//checking for the stopping place    
-    slowDown = true;      
-    stopServo();
-    }
-  }else if(angleChange <= 30){
-    if(numTicksCounted > (numTicksNeeded - 0) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    }
-  }else if(angleChange <= 60){
-    if(numTicksCounted > (numTicksNeeded - 10) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    }
-  }else if(angleChange <= 90){
-    if(numTicksCounted > (numTicksNeeded - 40) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    }
-  }else if(angleChange <= 135){
-    if(numTicksCounted > (numTicksNeeded - 80) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    }
-  }else if(angleChange <= 180){
-    if(numTicksCounted > (numTicksNeeded - 93) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    }
-  }else if(angleChange <= 270){
-    if(numTicksCounted > (numTicksNeeded - 140) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    }
-  }else if(angleChange <= 360){
-    if(numTicksCounted > (numTicksNeeded - 180) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    }
-  }else if(angleChange <= 540){
-   if(numTicksCounted > (numTicksNeeded - 150) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    } 
+  IRoutA = digitalRead(IRA);
+  IRoutB = digitalRead(IRB);
+
+  if(IRoutA != IRoutB){
+    numTicksCounted++;
   }else{
-    if(numTicksCounted > (numTicksNeeded - 400) && slowDown == false){//checking for the stopping place    
-      slowDown = true;      
-      stopServo();
-    } 
-  }
+    numTicksCounted--;
+  }  
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -194,4 +93,23 @@ void setVelocity(float velocity){                             //sets the velocit
     analogWrite(pinA, (127-velocity)*2);
   }
   return;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+float getAngle(int count){
+  angle = count * 1.2; //1.2 degrees per tick
+//  Serial.print("Angle: ");
+//  Serial.println(angle);
+  return angle;
+  
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+float pControl(float desiredAngle, float angle){
+  error = desiredAngle - angle;
+  output = (error*Kp);
+  return output;
 }
